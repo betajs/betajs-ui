5963e1ca0c073457fb6662b8fdf3804e81f004cd
Oliver Friedmann <oliverfriedmann@Olivers-MacBook-Pro.local>
2017-01-08 20:57:44 -0500

    Typo

--- dist/betajs-ui-noscoped.js
+++ dist/betajs-ui-noscoped.js
@@ -1,5 +1,5 @@
 /*!
-betajs-ui - v1.0.22 - 2017-01-08
+betajs-ui - v1.0.23 - 2017-01-08
 Copyright (c) Victor Lingenthal,Oliver Friedmann
 Apache-2.0 Software License.
 */
@@ -14,7 +14,7 @@ Scoped.binding('dynamics', 'global:BetaJS.Dynamics');
 Scoped.define("module:", function () {
 	return {
     "guid": "ff8d5222-1ae4-4719-b842-1dedb9162bc0",
-    "version": "76.1483926925799"
+    "version": "77.1483927057962"
 };
 });
 Scoped.assumeVersion('base:version', 474);
